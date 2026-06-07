d2f639cedfc679d8e8bae37b12327b5f173f6742
Pabilo8 <pabilo1.PG@gmail.com>
2026-06-07 21:08:46 +0200

    Modified Device and DeviceData

--- src/types/DeviceData.tsx
+++ src/types/DeviceData.tsx
@@ -1,6 +1,17 @@
 export type DeviceData = {
   id: number;
+
   humidity: number;
+  humidity_raw: number;
+
   lum: number;
+  lum_ch0: number;
+  lum_ch1: number;
+
   openValve: boolean;
+
+  lastSeen?: string;
+  requestedValveOpen?: boolean;
+  requestedValveTime?: number;
+  lastCommandAt?: string;
 };
